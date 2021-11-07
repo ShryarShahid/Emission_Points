2dce3f59f88ecfd68d786fa28eea8e75f5293b8b
Mubashir338 <79444924+Mubashir338@users.noreply.github.com>
2021-11-07 23:29:19 +0500

    java file

--- app/src/main/java/com/example/emissionpoints/MainActivity.java
+++ app/src/main/java/com/example/emissionpoints/MainActivity.java
@@ -1,14 +1,36 @@
+
 package com.example.emissionpoints;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/Mubashir338/Emission_Points");
+            }
+
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
     }
 }
